04f0826ecbf509087c9694548672b6b82d68720a
Md Rakibul Hasan <rakibulhasanshuvo206@gmail.com>
2025-02-17 15:01:03 +0600

    template filtering

--- project3/frist_app/views.py
+++ project3/frist_app/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-
+import datetime
 # Create your views here.
 def home(request):
-    d={'author': 'Rahim', 'age' : 15, 'list' : [1,2,3,4,5], 'courses' : [
+    d={'author': 'Rahim', 'age' : 15, 'list' : ['Python', 'is', 'best'], 'birthday' : datetime.datetime.now(),'val':24 ,'courses' : [
         {
             'id' : 1,
             'name' : 'python',
@@ -19,5 +19,5 @@ def home(request):
             'fee' : 1200
         }
 
-        ]}
+        ], 'name': 'daffodil', 'myname' : 'S h u v o', 'firstAndLast' : ['Shuvo', 'Aysha', 'Priya', 'Halima', 'Shopna', 'Zaidur'], 'university' : 'daffodil international university'}
     return render(request, 'home.html', d);
