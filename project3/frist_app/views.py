57fa984775fcb5895b09eec480a838100d2c2401
Md Rakibul Hasan <rakibulhasanshuvo206@gmail.com>
2025-02-17 13:32:11 +0600

    loop and conditional statement added

--- project3/frist_app/views.py
+++ project3/frist_app/views.py
@@ -2,5 +2,22 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    d={'author': 'Rahim', 'age' : 20, 'list' : [1,2,3,4,5]}
+    d={'author': 'Rahim', 'age' : 15, 'list' : [1,2,3,4,5], 'courses' : [
+        {
+            'id' : 1,
+            'name' : 'python',
+            'fee' : 5000
+        },
+        {
+            'id' : 2,
+            'name' : 'Django',
+            'fee' : 10000
+        },
+        {
+            'id' : 3,
+            'name' : 'C programming',
+            'fee' : 1200
+        }
+
+        ]}
     return render(request, 'home.html', d);
